e590547e215cbf255f4a2c41e48ce05a96066474
andrei <ablajievschii@syncast.ai>
2022-05-11 01:03:34 -0700

    Setup app navigation; Added main and details fragments stubs

---
+++ app/src/main/java/com/example/android/tmdbdemo1/MovieDetailsFragment.kt
@@ -0,0 +1,18 @@
+package com.example.android.tmdbdemo1
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class MovieDetailsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_movie_details, container, false)
+    }
+}
---
+++ app/src/main/java/com/example/android/tmdbdemo1/PopularMoviesFragment.kt
@@ -0,0 +1,39 @@
+package com.example.android.tmdbdemo1
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.android.tmdbdemo1.databinding.FragmentPopularMoviesBinding
+
+class PopularMoviesFragment : Fragment() {
+
+    private var _binding: FragmentPopularMoviesBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentPopularMoviesBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.bGoToDetails.setOnClickListener {
+            val action = PopularMoviesFragmentDirections.actionPopularMoviesFragmentToMovieDetailsFragment()
+            findNavController().navigate(action)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
