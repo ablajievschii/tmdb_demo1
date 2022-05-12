fd5e404e7016c0b6323f5a64ba6e9d681b7f115b
andrei <ablajievschii@syncast.ai>
2022-05-12 12:55:42 -0700

    Updated detail layout

--- app/src/main/java/com/example/android/tmdbdemo1/ui/details/MovieDetailsFragment.kt
+++ app/src/main/java/com/example/android/tmdbdemo1/ui/details/MovieDetailsFragment.kt
@@ -6,19 +6,26 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.example.android.tmdbdemo1.R
+import com.example.android.tmdbdemo1.databinding.FragmentMovieDetailsBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MovieDetailsFragment : Fragment() {
 
     private val viewModel by viewModels<MovieDetailsViewModel>()
+    private var _binding: FragmentMovieDetailsBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_movie_details, container, false)
+    ): View {
+        _binding = FragmentMovieDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 }
--- app/src/main/java/com/example/android/tmdbdemo1/ui/details/MovieDetailsViewModel.kt
+++ app/src/main/java/com/example/android/tmdbdemo1/ui/details/MovieDetailsViewModel.kt
@@ -1,9 +1,18 @@
 package com.example.android.tmdbdemo1.ui.details
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class MovieDetailsViewModel @Inject constructor(): ViewModel() {
+
+    private val _overview = MutableSharedFlow<String>()
+    val overview: LiveData<String> = _overview.asLiveData()
+
+    private val _posterPath = MutableSharedFlow<String>()
+    val posterPath: LiveData<String> = _posterPath.asLiveData()
 }
