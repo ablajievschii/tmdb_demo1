c417a44667c43cc7b24167d9aadfbc659369504c
andrei <ablajievschii@syncast.ai>
2022-05-12 15:04:01 -0700

    Implemented basic genres chips UI

--- app/src/main/java/com/example/android/tmdbdemo1/ui/ItemsAdapter.kt
+++ app/src/main/java/com/example/android/tmdbdemo1/ui/ItemsAdapter.kt
@@ -1,14 +1,18 @@
 package com.example.android.tmdbdemo1.ui
 
+import android.content.Context
+import android.content.res.ColorStateList
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.example.android.tmdbdemo1.R
 import com.example.android.tmdbdemo1.databinding.DiscoverMovieListItemBinding
-import com.example.android.tmdbdemo1.model.DiscoverMovie
 import com.example.android.tmdbdemo1.ui.UiModel.DiscoverMovieItem
+import com.google.android.material.chip.Chip
 import javax.inject.Inject
 
 class ItemsAdapter @Inject constructor() : PagingDataAdapter<UiModel, ItemsViewHolder>(MovieComparator) {
@@ -38,6 +42,17 @@ class ItemsViewHolder(private val binding: DiscoverMovieListItemBinding) : Recyc
 
     fun bind(item: DiscoverMovieItem) {
         binding.movie = item.movie
+//        binding.genres = item.genres?.joinToString(",")
+        val context = binding.root.context
+        val textColor = ContextCompat.getColor(context, R.color.white)
+        if (binding.genreChips.childCount == 0) { // add children once, assume genres does not change between binds
+            item.genres?.map { genre ->
+                binding.genreChips.addView(
+                    getGenreChip(context, genre, textColor)
+                )
+            }
+        }
+
         binding.executePendingBindings()
     }
 
@@ -47,6 +62,15 @@ class ItemsViewHolder(private val binding: DiscoverMovieListItemBinding) : Recyc
             val binding = DiscoverMovieListItemBinding.inflate(layoutInflater, parent, false)
             return ItemsViewHolder(binding)
         }
+    }
+}
+
+fun getGenreChip(context: Context, genre: String, textColor: Int): Chip {
+    return Chip(context).apply {
+        setEnsureMinTouchTargetSize(false)
+        text = genre
+        setChipBackgroundColorResource(R.color.dark_blue_700)
+        setTextColor(textColor)
     }
 }
 
