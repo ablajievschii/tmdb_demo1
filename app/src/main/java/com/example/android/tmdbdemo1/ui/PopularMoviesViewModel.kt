57b67944d9a379c6f8f6fde73399f82b84d36c7c
andrei <ablajievschii@syncast.ai>
2022-05-11 20:47:53 -0700

    Implemented PopularMoviesViewModel to load and expose movies data as a flow

--- app/src/main/java/com/example/android/tmdbdemo1/ui/PopularMoviesViewModel.kt
+++ app/src/main/java/com/example/android/tmdbdemo1/ui/PopularMoviesViewModel.kt
@@ -1,9 +1,56 @@
 package com.example.android.tmdbdemo1.ui
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import androidx.paging.map
+import com.example.android.tmdbdemo1.data.DiscoverMoviesRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@OptIn(ExperimentalCoroutinesApi::class)
 @HiltViewModel
-class PopularMoviesViewModel @Inject constructor(): ViewModel() {
+class PopularMoviesViewModel @Inject constructor(
+    private val repository: DiscoverMoviesRepository
+): ViewModel() {
+
+    val pagingData: Flow<PagingData<UiModel>>
+
+    /**
+     * UI Action processor
+     */
+    val accept: (UiAction) -> Unit
+
+    init {
+        // action router
+        val sharedFlow = MutableSharedFlow<UiAction>()
+
+        pagingData = sharedFlow
+            .filterIsInstance<UiAction.LoadMore>()
+            .distinctUntilChanged()
+            .flatMapLatest {
+                loadMovies()
+            }
+            .cachedIn(viewModelScope) // avoid data re-fetch on configuration change
+
+        accept = { action ->
+            viewModelScope.launch {
+                sharedFlow.emit(action)
+            }
+        }
+    }
+
+    private fun loadMovies(): Flow<PagingData<UiModel>> {
+        return repository.discoverMoviesFlow().map { pagingData ->
+            pagingData.map { UiModel.DiscoverMovieItem(it) }
+        }
+    }
+}
+
+sealed class UiAction {
+    class LoadMore() : UiAction()
 }
---
+++ app/src/main/java/com/example/android/tmdbdemo1/ui/UiModel.kt
@@ -0,0 +1,8 @@
+package com.example.android.tmdbdemo1.ui
+
+import com.example.android.tmdbdemo1.model.DiscoverMovie
+
+// There might be more than one item type, which will represent different RecyclerView list items
+sealed class UiModel {
+    data class DiscoverMovieItem(val movie: DiscoverMovie) : UiModel()
+}
