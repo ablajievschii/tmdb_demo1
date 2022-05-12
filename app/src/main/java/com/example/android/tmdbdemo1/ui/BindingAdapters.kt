5e7689f57669d136fe527a32676c04c6d605bc00
andrei <ablajievschii@syncast.ai>
2022-05-12 00:24:06 -0700

    Implemented release date formatting

--- app/src/main/java/com/example/android/tmdbdemo1/ui/BindingAdapters.kt
+++ app/src/main/java/com/example/android/tmdbdemo1/ui/BindingAdapters.kt
@@ -1,14 +1,32 @@
 package com.example.android.tmdbdemo1.ui
 
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
-import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import com.example.android.tmdbdemo1.api.tmdbImageUrl
+import timber.log.Timber
+import java.text.SimpleDateFormat
+import java.util.*
 
 @BindingAdapter("app:posterPathThumb")
-fun bindItems(imageView: ImageView, path: String?) {
+fun bindPosterPathThumb(imageView: ImageView, path: String?) {
     path?.let {
         imageView.load(tmdbImageUrl(path, "w342")) // FIXME Code smell, magic number :)
+    }
+}
+
+@BindingAdapter("formatDate")
+fun bindFormatDate(textView: TextView, value: String?) {
+    val df = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+    val formatter = SimpleDateFormat("MMM dd, yyyy", Locale.getDefault())
+    textView.text = value?.let { dateString ->
+        try {
+            df.parse(dateString)?.let { formatter.format(it) }
+        } catch (ex: Exception) {
+            // this is not critical error, so just log it as a warning
+            Timber.w("Failed to parse the date")
+            ""
+        }
     }
 }
